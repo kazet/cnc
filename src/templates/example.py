2a1a15a28972e5ffc5a210b613ab047978b2f25a
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2019-01-13 18:08:52 +0000

    print() -> emit()

--- src/templates/example.py
+++ src/templates/example.py
@@ -1,7 +1,10 @@
 # Enter machine commands here
-# Use any Python that prints G-code
-# You may browse available modules at the bottom of this page
+# Use any Python that creates GCode strings
+# and calls emit() on them: e.g. emit("G91")
+#
+# You may browse available GCode modules
+# at the bottom of this page
 
 from gcode_modules.snowman import code as snowman
 
-print(translate_and_scale(snowman(), scale=0.1))
+emit(translate_and_scale(snowman(), scale=0.1))
--- src/utils/python_to_gcode.py
+++ src/utils/python_to_gcode.py
@@ -1,22 +1,8 @@
 import os
-import sys
-from io import StringIO
-import contextlib
-
 
 # We want the following utilities to be visible to the executed code
 # so that it is able to call them
 from utils.translate_and_scale import translate_and_scale  # noqa
-
-
-@contextlib.contextmanager
-def stdoutIO(stdout=None):
-    old = sys.stdout
-    if stdout is None:
-        stdout = StringIO()
-    sys.stdout = stdout
-    yield stdout
-    sys.stdout = old
 
 
 def load_example(name):
@@ -33,6 +19,9 @@ def load_example(name):
 
 
 def python_to_gcode(code):
-    with stdoutIO() as s:
-        exec(code)
-        return s.getvalue()
+    def emit(s):
+        emit.result_gcode += s
+    emit.result_gcode = ''
+
+    exec(code)
+    return emit.result_gcode
